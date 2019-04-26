4965f14a09c52553f03a0d30096a4ca356e3a906
Ben Vazzana <bvazzana672@kirksville.k12.mo.us>
2019-04-26 17:00:11 -0500

    Added key input debugging

--- src/com/tetrisbot/input/KeyInput.java
+++ src/com/tetrisbot/input/KeyInput.java
@@ -1,4 +1,25 @@
 package com.tetrisbot.input;
 
-public class KeyInput {
+import com.tetrisbot.game.Game;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter {
+
+    Game game;
+
+    public KeyInput(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("[DEBUG]: You pressed the '" + e.getKeyChar() + "' key.");
+    }
+
+    public void keyReleased(KeyEvent e) {
+        System.out.println("[DEBUG]: You released the '" + e.getKeyChar() + "' key.");
+    }
+
 }
