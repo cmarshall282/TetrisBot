89d6b050aafacd13bb98d68f68accaf1f10167fe
Ben <bvazzana672@kirksville.k12.mo.us>
2019-04-26 22:16:55 -0500

    Fixed multicolored piece bug

--- src/com/tetrisbot/gameobjects/tetrispieces/IBlock.java
+++ src/com/tetrisbot/gameobjects/tetrispieces/IBlock.java
@@ -1,6 +1,7 @@
 package com.tetrisbot.gameobjects.tetrispieces;
 
 import com.tetrisbot.gameobjects.Block;
+import com.tetrisbot.gameobjects.BlockColor;
 import com.tetrisbot.utils.TetrisRandom;
 
 import java.util.Random;
@@ -10,8 +11,9 @@ public class IBlock extends BlockTemplate {
     public IBlock(Random r) {
         super();
         rotationState = 0;
+        BlockColor c = TetrisRandom.chooseColor(r);
         for(int i = 0; i < blocks.length; i++) {
-            blocks[i] = new Block(TetrisRandom.chooseColor(r), 0, 0);
+            blocks[i] = new Block(c, 0, 0);
         }
         blocks[0].setPerm(0, 0);
         blocks[1].setPerm(1, 0);
@@ -33,9 +35,9 @@ public class IBlock extends BlockTemplate {
                 break;
             case 1:
                 blocks[0].setPerm(0, 0);
-                blocks[0].setPerm(0, 1);
-                blocks[0].setPerm(0, 2);
-                blocks[0].setPerm(0, 3);
+                blocks[1].setPerm(0, 1);
+                blocks[2].setPerm(0, 2);
+                blocks[3].setPerm(0, 3);
                 break;
         }
     }
